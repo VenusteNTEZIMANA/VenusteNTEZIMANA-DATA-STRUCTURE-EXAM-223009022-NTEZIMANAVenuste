b478c9581f355588cdae7d6fa687b26c7277ffa8
VenusteNTEZIMANA <ntezimanavenuste38@gmail.com>
2025-02-21 10:06:37 +0200

    Add files via upload

---
+++ OOP/ex2.cpp
@@ -0,0 +1,7 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int age=25;
+    cout<<"the age is"<<age <<endl;
+    return 0;
+}
---
+++ OOP/ex3.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+int main(){
+int age = 30;
+float pi = 3.14;
+double num = 3.1415926535;
+char letter = 'A';
+bool isHappy = true;
+string name = "Alice";
+cout<<"the age is "<<age<<endl;
+cout<<"the pi is "<<pi<<endl;
+cout<<"the number  is "<<num<<endl;
+cout<<"the letter is "<<letter<<endl;
+cout<<"the ishappy is "<<isHappy<<endl;
+cout<<"the name is "<<name<<endl;
+return 0;
+
+}
